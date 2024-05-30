af978add85a9467826fbd11cc0c6657c1a47e22b
Ujjwal Neupane <itsmeujjwal725@gmail.com>
2024-05-30 16:39:51 +0545

    Serializers and views created

---
+++ base/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import *
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Department
+        fields = '__all__'
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+class ProductCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductCategory
+        fields = '__all__'
+class SupplierSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Supplier
+        fields = '__all__'
+class PurchaseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Purchase
+        fields = '__all__'
+        
--- base/views.py
+++ base/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
-
+from rest_framework.viewsets import ModelViewSet
+from .models import *
+from .serializers import *
 # Create your views here.
 
+class DepartmentApiView(ModelViewSet):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+class ProductApiView(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+class ProductCategoryApiView(ModelViewSet):
+    queryset = ProductCategory.objects.all()
+    serializer_class = ProductCategorySerializer
+class SupplierApiView(ModelViewSet):
+    queryset = Supplier.objects.all()
+    serializer_class = SupplierSerializer
+class PurchaseApiView(ModelViewSet):
+    queryset = Purchase.objects.all()
+    serializer_class = PurchaseSerializer
+    
     
